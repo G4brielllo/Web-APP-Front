ed1e9986d002512f3985322ff01b317a302cdd99
G4brielllo <gabrieltarasiuk3@gmail.com>
2024-11-02 13:40:35 +0100

    Bug fix

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,14 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
