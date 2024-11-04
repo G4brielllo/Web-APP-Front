46497a53980864ecb6ab27ef6e3a50de84dfa087
G4brielllo <gabrieltarasiuk3@gmail.com>
2024-11-04 21:24:33 +0100

    Configure fetching clients

--- frontend/src/components/ListClients.vue
+++ frontend/src/components/ListClients.vue
@@ -16,6 +16,8 @@
         </v-toolbar>
         <v-card-text>
           <v-data-table
+            :headers="headers"
+            :items="clients"
             :search="search"
           >
             <template v-slot:[`item.logo`]="{ item }">
@@ -40,12 +42,15 @@
 </template>
 
 <script>
+import axios from "axios";
+axios.defaults.baseURL = "http://127.0.0.1:8000/";
 export default {
   name: "ListClients",
 
   data() {
     return {
       search: "",
+      headers: this.getHeaders(),
       userRole: null,
       isAdmin: false,
       clients: [],
@@ -53,15 +58,53 @@ export default {
     };
   },
   methods: {
+    async fetchClients() {
+      try {
+        const response = await axios.get("http://127.0.0.1:8000/clients");
+        this.clients = response.data.map((client) => ({
+          id: client.id,
+          name: client.name,
+          logo: client.logo,
+          country: client.country,
+          formatted_created_at: this.formatDate(client.created_at),
+
+        }));
+      } catch (error) {
+        console.error("Error fetching clients:", error);
+      }
+    },
+    formatDate(date) {
+      const options = { day: "numeric", month: "short", year: "numeric" };
+      return new Date(date).toLocaleDateString("pl-PL", options);
+    },
+    getHeaders() {
+      const baseHeaders = [
+        { text: "L.p.", value: "id" },
+        { text: "Nazwa", value: "name" },
+        { text: "Logotyp", value: "logo" },
+        { text: "Kraj", value: "country" },
+        { text: "Data dodania", value: "formatted_created_at" },
+      ];
+
+      if (this.isAdmin) {
+        baseHeaders.push({ text: "Akcje", value: "actions", sortable: false });
+      }
+
+      return baseHeaders;
+    },
     returnToHomePage() {
       this.$router.push("/");
     },
+  },
+  created() {
+    this.fetchClients();
   },
 };
 </script>
 
 <style scoped>
 @import "~@mdi/font/css/materialdesignicons.css";
+
 
 .fill-height {
   height: 100vh;
