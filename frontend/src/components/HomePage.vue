20dde91faabdeac1182eb375734c23cab1e409a0
G4brielllo <gabrieltarasiuk3@gmail.com>
2024-12-08 12:51:45 +0100

    add fetchUserData and logout methods in HomePage component

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -7,9 +7,16 @@
         </v-col>
         <v-col>
           <v-container>
-            <v-row justify="center">
+            <v-row justify="center" v-if="welcomeAlert">
               <v-col cols="12" md="8" lg="6">
-                
+                <v-alert
+                  type="success"
+                  dismissible
+                  transition="scale-transition"
+                  v-model="welcomeAlert"
+                >
+                  Witaj, {{ userName }}!
+                </v-alert>
               </v-col>
             </v-row>
             <v-row justify="center">
@@ -71,9 +78,13 @@ import user from "@/assets/user.png";
 import project from "@/assets/project.png";
 import estimation from "@/assets/budgeting.png";
 import NavigationDrawer from "@/components/NavigationDrawer.vue";
+import axios from "axios";
 
+import CryptoJS from "crypto-js";
+
+const encryptionKey = "V3ryS3cur3K3y#2024!";
 export default {
-  name: 'HomePage',
+  name: "HomePage",
   components: {
     NavigationDrawer,
   },
@@ -83,24 +94,131 @@ export default {
       project: project,
       estimation: estimation,
       isHovered: false,
+      welcomeAlert: false,
       userName: "",
+      alertShown: false,
+      token: false,
     };
   },
-  methods:{
+  created() {
+    this.fetchUserData();
+  },
+  methods: {
+    async fetchUserData() {
+      try {
+        const encryptedData = localStorage.getItem(encryptionKey);
+        if (!encryptedData) {
+          console.error("Encrypted data not found in localStorage.");
+          return;
+        }
+
+        let user_information;
+        try {
+          const bytes = CryptoJS.AES.decrypt(encryptedData, encryptionKey);
+          user_information = bytes.toString(CryptoJS.enc.Utf8);
+          if (!user_information) {
+            console.error("Decrypted data is empty or invalid.");
+            return;
+          }
+        } catch (error) {
+          console.error("Error decrypting data:", error);
+          return;
+        }
+
+        const token = localStorage.getItem("jwt_token");
+        if (!token) {
+          console.error("JWT token is missing. Redirecting to login page...");
+          this.$router.push("/login");
+          return;
+        }
+
+        const response = await axios.get("http://127.0.0.1:8000/users", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        if (response.status === 200) {
+          if (!localStorage.getItem("alertShown")) {
+            this.welcomeAlert = true;
+            localStorage.setItem("alertShown", "true");
+            setTimeout(() => {
+              this.welcomeAlert = false;
+            }, 3000);
+          }
+        } else {
+          console.error("Unexpected response status:", response.status);
+        }
+      } catch (error) {
+        console.error(
+          "Error fetching user data:",
+          error.response || error.message
+        );
+      }
+    },
+
+    async logout() {
+      try {
+        const token = localStorage.getItem("jwt_token");
+        console.log(token.value);
+        if (!token) {
+          console.error("No token found. User is not logged in.");
+          return;
+        }
+
+        const response = await axios.post(
+          "http://127.0.0.1:8000/logout",
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        if (response.status === 200) {
+          console.log("Logout successful:", response.data);
+          localStorage.removeItem("jwt_token");
+          this.$router.push("/login");
+          localStorage.removeItem("alertShown");
+        } else {
+          console.error("Logout failed:", response.data);
+        }
+      } catch (error) {
+        console.error("Logout error:", error);
+      }
+    },
     goToListClients() {
-
+      const token = localStorage.getItem("jwt_token");
+      if (token) {
         this.$router.push("/listClients");
-
+      } else {
+        this.goToLogin();
+      }
     },
     goToListProjects() {
-
+      const token = localStorage.getItem("jwt_token");
+      if (token) {
         this.$router.push("/listProjects");
-
+      } else {
+        this.goToLogin();
+      }
     },
     goToListEstimations() {
+      const token = localStorage.getItem("jwt_token");
+      if (token) {
         this.$router.push("/listEstimations");
+      } else {
+        this.goToLogin();
+      }
     },
-  }
+  },
+  goToLogin() {
+    this.$router.push("/login");
+  },
+  goToRegister() {
+    this.$router.push("/register");
+  },
 };
 </script>
 
@@ -146,7 +264,4 @@ export default {
 .position-relative {
   position: relative;
 }
-
-
 </style>
-
